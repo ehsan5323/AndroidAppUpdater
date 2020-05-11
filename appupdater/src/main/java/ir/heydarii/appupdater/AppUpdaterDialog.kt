dbea6b571dacf1d7d90105596fe5724cc54ead0e
ehsan ahmadi <ehsan5323@gmail.com>
2020-05-11 12:33:15 +0430

    test comment

--- appupdater/src/main/java/ir/heydarii/appupdater/AppUpdaterDialog.kt
+++ appupdater/src/main/java/ir/heydarii/appupdater/AppUpdaterDialog.kt
@@ -24,6 +24,7 @@ import kotlinx.android.synthetic.main.fragment_app_updater_dialog.*
 /**
  * Shows ForceUpdate Dialog Fragment
  */
+//testttttttttttttt
 class AppUpdaterDialog : DialogFragment() {
 
     override fun onCreateView(
